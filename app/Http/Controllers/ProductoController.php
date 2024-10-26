113d508860271e7837c44f78469d64535d99c338
MAB <mbellotto@gmail.com>
2024-10-26 12:01:31 -0300

    feat Add Productos resource feat Basic UI

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductoService;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+
+    private ProductoService $productoService;
+
+    public function __construct(ProductoService $productoService)
+    {
+        $this->productoService = $productoService;
+    }
+
+    public function index()
+    {
+        return view('productos.index');
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+
+}
